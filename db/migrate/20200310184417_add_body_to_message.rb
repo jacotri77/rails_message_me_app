fb30aa9143af1427eb9d887eb688a1e5e5dc3087
Erik Jacobsen <erikjacobsen@Eriks-MacBook-Pro.local>
2020-03-10 11:52:58 -0700

    adding in the Message resources - Model, migrations

---
+++ db/migrate/20200310184417_add_body_to_message.rb
@@ -0,0 +1,6 @@
+class AddBodyToMessage < ActiveRecord::Migration[5.2]
+  def change
+    add_column :messages, :body, :text
+    add_column :messages, :user_id, :integer
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "Erik", password: "password")
+User.create(username: "Ali", password: "password")
+User.create(username: "Alex", password: "password")
+User.create(username: "Yazzie", password: "password")
+User.create(username: "Roman", password: "password")
